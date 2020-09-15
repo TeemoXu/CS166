6c2233fad800a4aa50cca0dd49118f8fd4a2af2a
TeemoXu <pxu3@uvm.edu>
2020-09-16 00:04:42 +0800

    first assignment

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # CS166
+## assignment 1
+Write a Python program: Create a login and menu to a company intranet system that requires users (employees) to enter a 
+username and password in order to view a menu of options (such as Time Reporting, Accounting, IT Helpdesk, Engineering 
+Documents, etc.). 
+
+Technical requirements:
+
+1. Plaintext usernames/passwords/access level stored in a csv (or text) file
+2. Create three different access levels (roles for different users). For example, User A should have 
+access to all menu items ('admin' access), while User B has limited access (no Accounting or Engineering 
+Documents), etc. 
+3. Once logged in the user should be able to select different menu options with a number input (for 
+example, "press 1 for the Time Reporting area", "press 2 for the Accounting area", etc.).
+4. When a user enters a menu area they have access to, a simple message similar to 'You have now 
+accessed the accounting application' is sufficient to indicate a successful demonstration of the access 
+control (no need to build out any actual accounting functionality). Likewise, if a user does not have 
+the appropriate access level to view a menu area, the program should display a 'You are not authorized 
+to access this area.' message and provide an option to return them to the main menu.
+5. Good programming practices: Must adhere to Python PEP8 style guide (refer to style guide reference 
+PDF posted in this module). Reasonable amount of error/exception handling. Must organize code into 
+functions (no duplicate code!). Well-documented. Written in Python 3. No GUI, this should be a command 
+menu driven program.
+
+### how to run the program
+make sure put main.py and user_info.csv in the same directory, and open command line tool, find the directory, and type 
+"python main.py".
+1. username: user_a, password: admin - can access to all applications
+2. username: user_b, password: limited1 - can access all except the last application
+3. username: user_c, password: limited2 - can access menu and time reporting
+
+make sure username and password are typed correctly, otherwise the program will terminate, after log in,
+there is no need to worry about the what to input, program will not terminate
+
+inside the program, there should be enough instruuction to run the program
--- main.py
+++ main.py
@@ -1,53 +1,116 @@
-'''
+"""
 Assignment 1
 Teemo Xu
 CS 166 / Fall 2020
 
-Write a Python program: Create a login and menu to a company intranet system that requires users (employees) to enter a
-username and password in order to view a menu of options (such as Time Reporting, Accounting, IT Helpdesk, Engineering
-Documents, etc.).
-
-Technical requirements:
-
-Plaintext usernames/passwords/access level stored in a csv (or text) file Create three different access levels (roles
-for different users). For example, User A should have access to all menu items ('admin' access), while User B has
-limited access (no Accounting or Engineering Documents), etc.
-Once logged in the user should be able to select different menu options with a number input (for example, "press 1 for
-the Time Reporting area", "press 2 for the Accounting area", etc.).
-When a user enters a menu area they have access to, a simple message similar to 'You have now accessed the accounting
-application' is sufficient to indicate a successful demonstration of the access control (no need to build out any actual
-accounting functionality). Likewise, if a user does not have the appropriate access level to view a menu area, the
-program should display a 'You are not authorized to access this area.' message and provide an option to return them to
-the main menu.
-Good programming practices: Must adhere to Python PEP8 style guide (refer to style guide reference PDF posted in this
-module). Reasonable amount of error/exception handling. Must organize code into functions (no duplicate code!).
-Well-documented. Written in Python 3. No GUI, this should be a command menu driven program.
-Please submit a compressed folder containing all of the files associated with your assignment, as well as instructions
-for testing your program. Note: you will be adding functionality to this system in a subsequent assignment, so take your
-time and plan out your design.
-'''
+This is a simple program for three default account use to access 4 unimplemented applications.
+First user need to type in the right user name and password
+Then user can type in 1-6 to access different application, only with granted permission
+"""
 
 import csv
 
+# function to check if user use one of the three default accounts
 def login(filename):
-    dic = {}
     try:
+        # use csv module to open any csv file
         with open(filename, 'r') as csv_file:
-            print("------------ Login ------------")
+            # store csv file into a list of dictionaries
             data = csv.reader(csv_file)
-            username = input("Enter your username: ").strip()
-            password = input("Enter your password: ").strip()
-            list_of_account = list(data)
-            for i in range(len(list_of_account)):
-                if list_of_account[i][0] == username and list_of_account[i][1] == password:
-                    print("login successful")
-                else:
-                    print("invalid username or password")
-                    break
+            for row in data:
+                user[row[0]] = {'password': row[1], 'access':row[2]}
+
+            # check if user use the right combo of account
+            if user[username]['password'] == password:
+                print('------- welcome, {} -------'.format(username))
+            else:
+                # wrong combination of username and password will terminate the program
+                print("Incorrect password, terminating program...")
+                exit()
     except FileNotFoundError:
+        # check if the csv file exists
         print("{} not found, program terminating...".format(filename))
         exit()
+    except KeyError:
+        # using invalid username will terminate the program
+        print("{} is not a valid username".format(username))
+        exit()
+
+# function to display menu
+def menu():
+    print('1. Press 1 for menu area')
+    print('2. Press 2 for Time Reporting area')
+    print('3. Press 3 for Accounting area')
+    print('4. Press 4 for IT Helpdesk area')
+    print('5. Press 5 for Engineering Documents area')
+    print('6. Press 6 to close the program')
+
+# function to check if user have the access to time reporting application
+def time_reporting():
+    level = user[username]['access']
+    if level == '1' or level == '2' or level == '3':
+        print("You are in the Time Reporting application\n")
+    else:
+        print("You are not authorized to access this area.\n")
+
+# function to check if user have the access to accounting application
+def accounting():
+    level = user[username]['access']
+    if level == '1' or level == '2' or level == '3':
+        print("You are in the Accounting application\n")
+    else:
+        print("You are not authorized to access this area.\n")
+
+# function to check if user have the access to IT helpdesk application
+def helpdesk():
+    level = user[username]['access']
+    if level == '1' or level == '2':
+        print("You are in the IT Helpdesk application\n")
+    else:
+        print("You are not authorized to access this area.\n")
+
+# function to check if user have the access to engineering document application
+def document():
+    level = user[username]['access']
+    if level == '1':
+        print("You are in the Engineering Documents application\n")
+    else:
+        print("You are not authorized to access this area.\n")
 
 
 if __name__ == "__main__":
+    user = {}
+    print("------------ Login ------------")
+    # take in user's input of their user name and password
+    username = input("Enter your username: ").strip()
+    password = input("Enter your password: ").strip()
     login("user_info.csv")
+    # display menu
+    menu()
+    menu_choice = 0
+    # while loop to make sure the program run properly, exit with 6 entered
+    while menu_choice != 6:
+        try:
+            menu_choice = int(input("What's you choice(1 for menu, 6 for exit): "))
+            if menu_choice == 1:
+                menu()
+            elif menu_choice == 2:
+                time_reporting()
+            elif menu_choice == 3:
+                accounting()
+            elif menu_choice == 4:
+                helpdesk()
+            elif menu_choice == 5:
+                document()
+            elif menu_choice == 6:
+                pass
+            else:
+                # avoid anything other than 1-6 entered
+                print("Invalid input.")
+        except ValueError:
+            # avoid anything other than number entered
+            print("Invalid input.")
+    print("Terminating...")
+
+
+
